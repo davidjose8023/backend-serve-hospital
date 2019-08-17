337f3dd731a1442f867d6ee11a0d89689a4f2838
davidjose8023 <davidjose8023@gmail.com>
2019-08-16 21:28:07 -0400

    Seccion 12 lista

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@semilla@dificil';
+
+//google
+module.exports.CLIENT_ID = '774878914879-162j2ho78134oomr08sgljn9gho0ov1u.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,128 @@ var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
+
 var SEED = require('../config/config').SEED;
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
 
 var login = express();
 
 var Usuario = require('../models/usuario');
 
+//====================================================
+// Autenticación de Google
+//====================================================
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+  }
+
+login.post('/google', async (req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+    .catch( e => {
+        return res.status(400).json({
+            ok: false,
+            mensaje: 'token no valido',
+            error: e.error
+        });
+    });
+
+    Usuario.findOne({email : googleUser.email }, (err, usuarioBD) => {
+        
+        if( err ) 
+        return res.status(500).json({
+            ok: false,
+            mensaje: 'Error al buscar usuario',
+            errors: err
+        });
+
+        if(usuarioBD){
+            if(!usuarioBD.google){
+
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe autenticarse normal'
+                });
+
+            }else{
+
+                return res.status(200).json({
+                    ok: true,
+                    token,
+                    usuario:usuarioBD,
+                    id: usuarioBD._id
+                });
+            }
+            
+        }else{// el usuario no exite hay que crearlo
+            
+            var usuario =  new Usuario();
+
+            usuario.email = googleUser.email;
+            usuario.nombre = googleUser.nombre;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) =>{
+          
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400}); // 4 horas
+                if( err ) 
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error crear usuario',
+                    errors: err
+                });
+                return res.status(200).json({
+                    ok: true,
+                    token,
+                    usuario:usuarioDB,
+                    id: usuarioDB._id
+                });
+            });
+
+        }
+
+        
+
+        
+    });
+
+    // return res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'exitoo',
+    //     googleUser
+
+    // });
+    
+
+});
+
+//====================================================
+// Autenticación Normal
+//====================================================
 
 
 login.post('/', (req, res) => {
@@ -45,7 +161,8 @@ login.post('/', (req, res) => {
         return res.status(200).json({
             ok: true,
             token,
-            usuarioBD
+            usuario:usuarioBD,
+            id: usuarioBD._id
         });
     });
 
